8866862a30c15f3d51ac008b620d235b99b94401
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-08-19 17:10:55 +0530

    Set dynamic rendering for AdminDashboard component to 'force-dynamic' to enhance data fetching capabilities.

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminDashboard() {
   const [districts, results, users] = await Promise.all([
     prisma.district.count(),
